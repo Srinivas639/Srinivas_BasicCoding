858ae14b75a20b969394affec356084b963ce472
Srinivas Margam <margamsrinivas111@gmail.com>
2023-12-20 20:45:18 -0500

    Basic Coding for logical building - 31 Problems

--- src/main/scala/BasicCoding.scala
+++ src/main/scala/BasicCoding.scala
@@ -466,6 +466,58 @@ object NestedLoop {
   }
 }
 
+//Pattern
+object Pattern{
+  def main(args: Array[String]): Unit = {
+    var num=1
+    for(i <- 1 to 5){
+      for(j <- 1 to i){
+        print(s"$num ")
+        num = num+1
+      }
+      println()
+    }
+  }
+}
+
+object NumberPattern {
+  def main(args: Array[String]): Unit = {
+
+    for (i <- 6 to 1 by -1) {
+      for (j <- 1 to i) {
+        print(s"$j ")
+      }
+      println()
+    }
+  }
+}
+
+object NumberPattern3 {
+  def main(args: Array[String]): Unit = {
+
+    for (i <- 6 to 1  by -1) {
+      for (j <- 1 to i) {
+        print(s"$i ")
+      }
+      println()
+    }
+  }
+}
+
+object NumberPattern6 {
+  def main(args: Array[String]): Unit = {
+
+    for (i <- 1 to 6) {
+      for (j <- 1 to i) {
+        print(s"$i ")
+      }
+      println()
+    }
+  }
+}
+
+
+
 
 
 
